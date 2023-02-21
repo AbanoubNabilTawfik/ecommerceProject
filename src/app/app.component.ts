822a2d3283e5839746f4b8091d9d5f032139ffc6
Abanoub Nabil Tawfik <abanoub.nabil2016@gmail.com>
2023-02-21 11:36:14 +0200

    End of section 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './shared/models/paging';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  title = 'Store Application';
+  constructor(private http:HttpClient){}
+  products:any[]=[];
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>("https://localhost:7125/api/product?pageSize=50").subscribe({
+      next :response=>{
+        console.log(response)
+        this.products=response.data
+      },
+      error:error=>console.log(error),
+      complete:()=>{
+        console.log("Request completed")
+        console.log("extra staments")
+      }
+    })
+    
+  }
 }
